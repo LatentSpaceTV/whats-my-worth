f50b8a6b76c35a7c67cde70d76b3027b454c61bb
Latent Space <LatentSpace@outlook.de>
2026-06-24 23:23:53 +0200

    Refactored App to be more modular and have clear lib.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/whats-my-worth/',
-})
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
+  };
+});
